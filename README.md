041e2966d55fc14a31466ee9178c529ef238c2f8
Maro <marowinse@outlook.com>
2017-10-30 15:39:28 +0100

    updates to readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+[![Build Status](https://travis-ci.org/Oghenemaro/recipes.svg?branch=database_setup)](https://travis-ci.org/Oghenemaro/recipes)
 # recipes
 A platform for food recipes
--- server/models/index.js
+++ server/models/index.js
@@ -23,9 +23,11 @@ fs
   .filter(file => 
     (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
+    const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
+
+
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
